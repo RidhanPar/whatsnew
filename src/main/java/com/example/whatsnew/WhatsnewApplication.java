fce2e6a9b59ce08af49423cb0ecccaa5f40465fd
RidhanParvendhan <ridhanparvendhan@gmail.com>
2023-05-20 14:45:34 +0300

    Initial commit

---
+++ src/main/java/com/example/whatsnew/WhatsnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whatsnew/WhatsnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
